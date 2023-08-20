9dd3bab962a41c2563540857e148dd333ed0a7e3
Freddiewrig <thecollegeace1@gmail.com>
2023-08-20 07:54:48 -0700

    printf

--- function.c
+++ function.c
@@ -2,16 +2,16 @@
 #include <stdarg.h>
 #include <stdio.h>
 /**
- * _printf -  prints output according to a format
- * @format: character string
- * @...: urguments for format
+ * _printf - produces output according to a format
+ * @format: string of characters
+ * @...: urguments of format
  * Return: count
  */
 int _printf(const char *format, ...)
 {
 	va_list args;
+	int num;
 	int count = 0;
-	const char *str;
 
 	va_start(args, format);
 	while (*format != '\0')
@@ -21,34 +21,21 @@ int _printf(const char *format, ...)
 			format++;
 			switch (*format)
 			{
-				case 'c':
-					putchar(va_arg(args, int));
-					count++;
-					break;
-				case 's':
-					str = va_arg(args, const char *);
-					while (*str != '\0')
-					{
-						putchar(*str);
-						str++;
-						count++;
-					}
-					break;
-				case '%':
-					putchar('%');
+				case 'd':
+				case 'i':
+					num = va_arg(args, int);
+					putchar('0' + num);
 					count++;
 					break;
 				default:
-					putchar('%');
 					putchar(*format);
-					count += 2;
+					count++;
 					break;
 			}
 		}
 		else
 		{
 			putchar(*format);
-			count++;
 		}
 		format++;
 	}
