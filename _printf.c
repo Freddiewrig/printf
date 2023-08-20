1f2146d8109ae141e5b3c75e7d17ffe0e70864b3
Freddiewrig <thecollegeace1@gmail.com>
2023-08-20 14:04:58 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -1,16 +1,20 @@
 #include "main.h"
 #include <stdarg.h>
+#include <unistd.h>
 #include <stdio.h>
+#define BUFF_SIZE 1024
+void print_buffer(char buffer[], int *buff_ind);
 /**
- * _printf -  prints output according to a format
- * @format: character string
- * @...: urguments for format
- * Return: count
+ * _printf - Printf function
+ * @format: format.
+ * Return: Printed chars.
  */
 int _printf(const char *format, ...)
 {
 	va_list args;
-	int count = 0;
+	const char *str;
+	int count = 0, buff_ind = 0;
+	char buffer[BUFF_SIZE];
 
 	va_start(args, format);
 	while (*format)
@@ -21,18 +25,30 @@ int _printf(const char *format, ...)
 			switch (*format)
 			{
 				case 'c':
+					print_buffer(buffer, &buff_ind);
 					putchar(va_arg(args, int));
 					count++;
 					break;
 				case 's':
-					putchar(*va_arg(args, const char *));
-					count++;
+					{
+						print_buffer(buffer, &buff_ind);
+						str = va_arg(args, const char *);
+
+						while (*str)
+						{
+							putchar(*str);
+							str++;
+							count++;
+						}
+					}
 					break;
 				case '%':
+					print_buffer(buffer, &buff_ind);
 					putchar('%');
 					count++;
 					break;
 				default:
+					print_buffer(buffer, &buff_ind);
 					putchar('%');
 					putchar(*format);
 					count += 2;
@@ -41,11 +57,26 @@ int _printf(const char *format, ...)
 		}
 		else
 		{
-			putchar(*format);
+			buffer[buff_ind++] = *format;
+			if (buff_ind == BUFF_SIZE)
+				print_buffer(buffer, &buff_ind);
 			count++;
 		}
 		format++;
 	}
+	print_buffer(buffer, &buff_ind);
 	va_end(args);
 	return (count);
+}
+/**
+ * print_buffer - Prints the contents of the buffer if it exist
+ * @buffer: Array of chars
+ * @buff_ind: Index at which to add next char, represents the length.
+ */
+void print_buffer(char buffer[], int *buff_ind)
+{
+	if (*buff_ind > 0)
+		write(1, &buffer[0], *buff_ind);
+
+	*buff_ind = 0;
 }
