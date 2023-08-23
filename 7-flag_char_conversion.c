bb23ec94c1a2ef462efcb9f381f5490118c771bb
smythlamar <caltiesmith41@gmail.com>
2023-08-23 01:49:24 -0700

    7-flag_char_conversion.c

--- 7-flag_char_conversion.c
+++ 7-flag_char_conversion.c
@@ -2,15 +2,37 @@
 #include <stdio.h>
 #include <stdarg.h>
 /**
- * _flag_conversion - sorts flag conversion
+ * _print_octal - sorts flag conversion
  * @args: arguments
  * @count: int printed
  **/
-void _flag_conversion(va_list args, int *count)
+void _print_octal(va_list args, int *count)
 {
 	 unsigned int num = va_arg(args, unsigned int);
 
 	 *count += printf("%#o", num);
-	 *count += printf("% d", num);
-	 *count += printf("%+d", num);
+}
+/**
+ * _print_integer - prints int
+ * @args: arguments
+ * @count: int printed
+ * Return: count
+ **/
+int _print_integer(va_list args, int *count)
+{
+	int num = va_arg(args, int);
+
+	*count += printf("% d", num);
+	return (*count);
+}
+/**
+ * _print_unsign - prints unsigned int
+ * @args: urguments
+ * @count: number of int printed
+ */
+void _print_unsign(va_list args, int *count)
+{
+	unsigned int num = va_arg(args, unsigned int);
+
+	*count += printf("%u", num);
 }
